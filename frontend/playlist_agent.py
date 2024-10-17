3e74d5a6a87c30abbfb247ce43db0c5505f12297
OleSeifert <74614364+OleSeifert@users.noreply.github.com>
2024-10-17 12:22:18 +0200

    Added a non-intrusive way to suggest actions to the user.

--- frontend/playlist_agent.py
+++ frontend/playlist_agent.py
@@ -1,6 +1,7 @@
 """Simplest possible agent that parrots back everything the user says."""
 
 import os
+import random
 import sqlite3
 
 import requests
@@ -34,6 +35,34 @@ class PlaylistAgent(Agent):
             os.path.join(os.path.dirname(__file__), "../data/final_database.db")
         )
         self.dbmanager = DatabaseManager(db_path)
+
+        self.commands_not_utilized = [
+            "add",
+            "delete",
+            "view",
+            "clear",
+            "help",
+            "Q1",
+            "Q2",
+            "Q3",
+            "Q4",
+            "Q5",
+            "Q6",
+        ]
+        self.counter = 1
+        self.command_mappings = {
+            "add": "Did you know you can add a song to the playlist by typing '/add <song_name>' or '/add <song_name> by <artist>' or '/add <artist> : <song_name>'?",
+            "delete": "Did you know you can delete a song from the playlist by typing '/delete <song_name>'?",
+            "view": "Did you know you can view the current playlist by typing '/view'?",
+            "clear": "Did you know you can clear the playlist by typing '/clear'?",
+            "help": "Did you know you can see all the available commands by typing '/help'?",
+            "Q1": "Did you know you can ask me when an album was released by typing 'When was album X released?'?",
+            "Q2": "Did you know you can ask me how many albums an artist has released by typing 'How many albums has artist Y released?'?",
+            "Q3": "Did you know you can ask me which album features a song by typing 'Which album features song X?'?",
+            "Q4": "Did you know you can ask me how many songs an album contains by typing 'How many songs does album X contain?'?",
+            "Q5": "Did you know you can ask me how long an album is by typing 'How long is album X?'?",
+            "Q6": "Did you know you can ask me the most popular song by an artist by typing 'What is the most popular song by artist Y?'?",
+        }
 
     def find_song_in_db(self, song_title, artist=None):
         """Find a song in the database."""
@@ -122,6 +151,18 @@ class PlaylistAgent(Agent):
             List containing the command and argument.
         """
         return text.split(" ", maxsplit=1)
+
+    def suggest_command_not_utilized(self):
+        """TODO"""
+        if self.counter % 5 == 0:
+            # Send a suggestion
+
+            pick = random.choice(self.commands_not_utilized)
+            suggestion = AnnotatedUtterance(
+                self.command_mappings[pick],
+                participant=DialogueParticipant.AGENT,
+            )
+            self._dialogue_connector.register_agent_utterance(suggestion)
 
     def add_song(self, command) -> None:
         # Estrai il titolo e l'artista dal comando usando la funzione parse_command
@@ -377,44 +418,70 @@ class PlaylistAgent(Agent):
             return
 
         if self.separate_utterance(utterance.text)[0] == "/add":
+            self.counter += 1
+            if "add" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("add")
             if len(self.separate_utterance(utterance.text)) < 2:
                 utterance = AnnotatedUtterance(
                     "Please provide the name of the song you want to add",
                     participant=DialogueParticipant.AGENT,
                 )
                 self._dialogue_connector.register_agent_utterance(utterance)
+                self.suggest_command_not_utilized()
                 return
             self.add_song(self.separate_utterance(utterance.text)[1])
+            self.suggest_command_not_utilized()
             return
 
         if self.separate_utterance(utterance.text)[0] == "/view":
+            self.counter += 1
+            if "view" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("view")
             self.view_playlist()
+            self.suggest_command_not_utilized()
             return
 
         if self.separate_utterance(utterance.text)[0] == "/clear":
+            self.counter += 1
+            if "clear" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("clear")
             self.clear_playlist()
+            self.suggest_command_not_utilized()
             return
 
         if self.separate_utterance(utterance.text)[0] == "/delete":
+            self.counter += 1
+            if "delete" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("delete")
             if len(self.separate_utterance(utterance.text)) < 2:
                 utterance = AnnotatedUtterance(
                     "Please provide the name of the song you want to delete",
                     participant=DialogueParticipant.AGENT,
                 )
                 self._dialogue_connector.register_agent_utterance(utterance)
+                self.suggest_command_not_utilized()
                 return
             self.delete_song(self.separate_utterance(utterance.text)[1])
+            self.suggest_command_not_utilized()
             return
 
         if self.separate_utterance(utterance.text)[0] == "/help":
+            self.counter += 1
+            if "help" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("help")
             self.welcome()
+            self.suggest_command_not_utilized()
             return
 
         # ---- Handle questions ----
         if "When was album" in utterance.text:  # A bit hard-coded but works for now
+            self.counter += 1
+            if "Q1" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q1")
             album_name = parsing.extract_album_from_question(utterance.text)
             if album_name:
                 self.find_album_release_date(album_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user album does not exist
             response = AnnotatedUtterance(
@@ -424,9 +491,13 @@ class PlaylistAgent(Agent):
             self.dialogue_connector.register_agent_utterance(response)
 
         if "How many albums has artist" in utterance.text:
+            self.counter += 1
+            if "Q2" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q2")
             artist_name = parsing.extract_artist_from_question(utterance.text)
             if artist_name:
                 self.find_number_of_albums_by_artist(artist_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user artist does not exist
             response = AnnotatedUtterance(
@@ -436,9 +507,13 @@ class PlaylistAgent(Agent):
             self.dialogue_connector.register_agent_utterance(response)
 
         if "Which album features song" in utterance.text:
+            self.counter += 1
+            if "Q3" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q3")
             song_name = parsing.extract_song_from_question_for_album(utterance.text)
             if song_name:
                 self.find_album_for_song(song_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user song does not exist
             response = AnnotatedUtterance(
@@ -448,9 +523,13 @@ class PlaylistAgent(Agent):
             self.dialogue_connector.register_agent_utterance(response)
 
         if "How many songs does album" in utterance.text:
+            self.counter += 1
+            if "Q4" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q4")
             album_name = parsing.extract_num_songs_on_album(utterance.text)
             if album_name:
                 self.how_many_songs_on_album(album_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user album does not exist
             response = AnnotatedUtterance(
@@ -460,9 +539,13 @@ class PlaylistAgent(Agent):
             self.dialogue_connector.register_agent_utterance(response)
 
         if "How long is album" in utterance.text:
+            self.counter += 1
+            if "Q5" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q5")
             album_name = parsing.extract_album_name_for_duration(utterance.text)
             if album_name:
                 self.how_long_is_album(album_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user album does not exist
             response = AnnotatedUtterance(
@@ -472,9 +555,13 @@ class PlaylistAgent(Agent):
             self.dialogue_connector.register_agent_utterance(response)
 
         if "What is the most popular song by" in utterance.text:
+            self.counter += 1
+            if "Q6" in self.commands_not_utilized:
+                self.commands_not_utilized.remove("Q6")
             artist_name = parsing.extract_artist_for_most_popular_song(utterance.text)
             if artist_name:
                 self.most_popular_song_by_artist(artist_name)
+                self.suggest_command_not_utilized()
                 return
             # Tell user artist does not exist
             response = AnnotatedUtterance(
