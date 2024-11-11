ac89c2bace8c5f01de0ada5e3eabcba9fe2eade5
MarcoBolo001 <143323325+MarcoBolo001@users.noreply.github.com>
2024-11-11 14:25:05 +0100

    delete_null

---
+++ data/delete_null.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# Connetti al database SQLite
+conn = sqlite3.connect('final_database.db')  # sostituisci con il percorso del tuo file DB
+cursor = conn.cursor()
+
+# Conta il numero di righe prima della cancellazione
+cursor.execute("SELECT COUNT(*) FROM music")  # sostituisci con il nome della tua tabella
+total_rows_before = cursor.fetchone()[0]
+
+# Esegui la cancellazione delle righe con acousticness null
+cursor.execute("DELETE FROM music WHERE acousticness IS NULL")  # sostituisci con il nome della tua tabella
+conn.commit()
+
+# Conta il numero di righe dopo la cancellazione
+cursor.execute("SELECT COUNT(*) FROM music")
+total_rows_after = cursor.fetchone()[0]
+
+# Calcola quante righe sono state eliminate
+deleted_rows = total_rows_before - total_rows_after
+
+# Mostra il numero di righe eliminate
+print(f"Numero di righe eliminate: {deleted_rows}")
+
+# Chiudi la connessione al DB
+conn.close()
